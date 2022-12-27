e371c384f6d40315bf86ff4eaf7595cfa594db75
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-12-27 14:42:02 +0900

    独自型をキーに使う

--- hoge/hoge.go
+++ hoge/hoge.go
@@ -2,12 +2,15 @@ package hoge
 
 import "context"
 
+// 独自構造体型の値をキーとして用いる
+type traceIDKey struct{}
+
 func SetTraceID(ctx context.Context, traceID int) context.Context {
-	return context.WithValue(ctx, "traceID", traceID)
+	return context.WithValue(ctx, traceIDKey{}, traceID)
 }
 
 func GetTraceID(ctx context.Context) int {
-	id := ctx.Value("traceID")
+	id := ctx.Value(traceIDKey{})
 	
 	if idInt, ok := id.(int); ok {
 		return idInt
